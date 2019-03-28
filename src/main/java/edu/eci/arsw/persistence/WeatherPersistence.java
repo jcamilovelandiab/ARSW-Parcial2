b36a2a0b518293e7a8419d8b861c65f2d8b045f6
jcamilovelandiab <jcamilovelandiab@gmail.com>
2019-03-28 18:35:39 -0500

    muestra los datos de la tabla responsive

--- src/main/java/edu/eci/arsw/persistence/WeatherPersistence.java
+++ src/main/java/edu/eci/arsw/persistence/WeatherPersistence.java
@@ -6,10 +6,27 @@ import edu.eci.arsw.entities.City;
 
 public interface WeatherPersistence {
 
+	/**
+	 * Consulta una ciudad dada su nombre
+	 * 
+	 * @param namecity
+	 * @return
+	 * @throws WeatherPersistenceException
+	 */
 	public City getCityByName(String namecity) throws WeatherPersistenceException;
 	
+	/**
+	 * Agrega la ciudad a persistence
+	 * @param city
+	 * @throws WeatherPersistenceException
+	 */
 	public void addCity(City city) throws WeatherPersistenceException; 
 	
+	
+	/**
+	 * Obtiene todas las ciudades registradas
+	 * @return
+	 */
 	public Set<City> getCities();
 	
 }
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -3,8 +3,21 @@ app=(function(){
 	return{
 		getWeatherByCityName:function(namecity){
 			return apiclient.getWeatherByCityName(namecity, function(city){
-				//var weather = city.weather;
-				console.log(city);
+				var weatherTxt = city.weather;
+				var info = JSON.parse(weatherTxt);
+				console.log(weatherTxt);
+				console.log(info);
+				
+				var table = $("#tableCity");
+				$("#tableCity > tr").remove();
+				
+				table.append('<tr>' + 
+						"<td> lat -> "+info.coord.lat+" ,lon -> "+info.coord.lon+"</td>" + 
+						"<td>"+info.weather[0].description+"</td>" + 
+						"<td>"+info.main.humidity+"</td>"+ 
+						"<td>"+info.main.pressure+"</td>"+
+						"<td>"+info.main.temp+"</td>"+
+						"</tr>");
 			})
 		}
 	}
